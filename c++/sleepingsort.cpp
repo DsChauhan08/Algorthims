eca15d6fdb7f04a6edc5b2dd5ea91d81bc1ec330
Dhananjay Singh Chauhan <101635962+DsChauhan08@users.noreply.github.com>
2023-12-24 21:48:01 +0530

    Create sleepingsort.cpp

---
+++ c++/sleepingsort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+// Function to perform sleep sort
+void sleepSort(int num) {
+    // Sleep for 'num' milliseconds
+    std::this_thread::sleep_for(std::chrono::milliseconds(num));
+    
+    // Print the sorted element after sleeping
+    std::cout << num << " ";
+}
+
+int main() {
+    // Vector to store input numbers
+    std::vector<int> numbers;
+
+    // Read numbers from command line arguments
+    for (int i = 1; i < argc; ++i) {
+        numbers.push_back(std::stoi(argv[i]));
+    }
+
+    // Create threads for each number
+    std::vector<std::thread> threads;
+    for (int num : numbers) {
+        threads.push_back(std::thread(sleepSort, num));
+    }
+
+    // Join threads to wait for their completion
+    for (std::thread& t : threads) {
+        t.join();
+    }
+
+    std::cout << std::endl;
+
+}
